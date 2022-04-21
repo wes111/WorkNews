7209dd45e186ce124e2cbe2025376f9711018a08
Wes Luntsford <wesleyluntsford@gmail.com>
2022-04-20 21:51:07 -0500

    Initial Commit

---
+++ WorkNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkNews
+//
+//  Created by Wesley Luntsford on 4/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorkNews/WorkNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkNewsApp.swift
+//  WorkNews
+//
+//  Created by Wesley Luntsford on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
